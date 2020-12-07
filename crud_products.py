ae35e27f88512baa0d3f543b496a69b729ce8b56
marcosdcl <marcosdouglasclima@gmail.com>
2020-12-07 15:29:23 -0300

    Create menu

--- crud_products.py
+++ crud_products.py
@@ -16,3 +16,16 @@ class Products:
             name, category, price = product.values()
             print('NAME: ' + name + ' CATEGORY: ' + category + ' PRICE: ' + str(price))
 
+product = Products()
+
+while True:
+    menu = int(input('\n1 - SHOW PRODUCTS\n2 - REGISTER A NEW PRODUCT\n0 - EXIT\n'))
+    if menu == 1:
+        product.show_products()
+    elif menu == 2:
+        product.set_product()
+    elif menu == 0:
+        break
+    else:
+        print('Please, type only 1 or 2')
+        continue
